75fb33b224852714df265f7baf9a277eb5f2d930
Karina809P <p.karina5555@gmail.com>
2025-06-16 17:54:44 +0300

    Оновлено fetch URL для GitHub Pages

--- src/redux/perfumes/perfumeAPI.ts
+++ src/redux/perfumes/perfumeAPI.ts
@@ -3,6 +3,6 @@ import { Perfume } from '../../types/perfume';  // імпортуємо тип
 import axios from 'axios';
 
 export const fetchPerfumes = async (): Promise<Perfume[]> => {
-  const response = await axios.get('/mock/db.json');
+  const response = await axios.get(`${process.env.PUBLIC_URL}/mock/db.json`);
   return response.data.perfumes;
 };
